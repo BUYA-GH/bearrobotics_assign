0536c26587e175889df4d164b438a91dc5b05809
BUYA-GH <baka3737baka@gmail.com>
2022-12-04 01:26:48 +0900

    add: Add model's admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Card, Account, Transition
 
 # Register your models here.
+class CardAdmin(admin.ModelAdmin):
+    search_fields = ('cardNum', )
+    list_display = ('cardNum', )
+
+class AccountAdmin(admin.ModelAdmin):
+    search_fields = ('accountNumber', 'author__cardNum', )
+    list_display = ('accountNumber', 'author__cardNum', 'balance', )
+
+class TransitionAdmin(admin.ModelAdmin):
+    search_fields = ('account__accountNumber', )
+    list_display = ('account__accountNumber', 'receivedPaid', 'transact_at', )
+
+admin.site.register(Card, CardAdmin)
+admin.site.register(Account, AccountAdmin)
+admin.site.register(Transition, TransitionAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,9 @@ class Card(models.Model):
     cardNum = models.CharField(max_length=16, unique=True)
     pinNum = models.CharField(max_length=4)
 
+    def __str__(self):
+        return f"{self.cardNum}"
+
 # Create your models here.
 class Account(models.Model):
     accountNumber = models.CharField(max_length=255, unique=True)
@@ -11,8 +14,14 @@ class Account(models.Model):
 
     author = models.OneToOneField(Card, on_delete=models.CASCADE, related_name="card_account")
 
+    def __str__(self):
+        return f"{self.accountNumber} / {self.author}"
+
 class Transition(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="account_transition")
     receivedPaid = models.IntegerField()
 
     transact_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f"{self.account} - {self.transact_at}"
