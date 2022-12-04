7372b942876a0f1795e084505ca2da88a6dc6ea9
BUYA-GH <baka3737baka@gmail.com>
2022-12-04 16:51:16 +0900

    add: Add trasition base view

--- accounts/serializers/trasition.py
+++ accounts/serializers/trasition.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
-from accounts.models import Card, Account
+from accounts.models import Card, Account, Transition
 
 class TransactionSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Transition
+        fields = ('account', 'balance', 'author',)
+        read_only_fields = ('account', 'balance', 'author',)
 
-class TrasactionTradeSerializer(TransactionSerializer):
-    pass
+class TrasactionWorkSerializer(TransactionSerializer):
+    
--- accounts/views/trasition.py
+++ accounts/views/trasition.py
@@ -2,14 +2,26 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 
+from accounts.serializers.trasition import *
+
 class BalanceView(APIView):
     def get(self, request):
-        pass
+        serializer = TransactionSerializer(request.data)
+        serializer.is_valid(raise_exception=True)
 
-class DepositView(APIView):
-    def post(self, request):
-        pass
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-class WithdrawView(APIView):
+class TransitionBaseView(APIView):
+    is_deposit = 1
+
     def post(self, request):
-        pass
+        serializer = TrasactionWorkSerializer(request.data, context={'is_deposit': self.is_deposit})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class DepositView(TransitionBaseView):
+    is_deposit = 1
+
+class WithdrawView(TransitionBaseView):
+    is_deposit = -1
